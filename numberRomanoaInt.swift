0665aac6bc40e87ce7eefecdd849ae777b930728
Andy Bryan Julca Tello <julcac137@gmail.com>
2024-01-20 17:53:17 -0500

    other example

--- numberRomanoaInt.swift
+++ numberRomanoaInt.swift
@@ -38,3 +38,37 @@ func solution(_ string:String) -> Int {
 }
 let result = solution("MMCDXXI")
 print(result)
+
+// forma 2
+
+func solution(_ string: String) -> Int {
+
+    let table: [Character: Int] = ["I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000]
+
+    var p = 0
+
+    let transform: (Character) -> Int = {
+      
+        let result = table[$0]!
+
+        print(type(of: $0))
+        if result > p {
+          
+            p = result
+        }
+
+        return result < p ? -result : result
+    }
+
+    let reversedArray = string.reversed()
+  
+    let transformedArray = reversedArray.map(transform)
+  
+    let finalResult = transformedArray.reduce(0, +)
+
+    return finalResult
+}
+
+let result = solution("MMCDXXI")
+
+print(result) // Imprime 2421
